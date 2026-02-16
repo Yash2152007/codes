f384c270d073bad515b4e0ddd733dc930f484c1d
Yash2152007 <2152007yash@gmail.com>
2026-02-17 03:02:08 +0530

    adding task 2

---
+++ assignment2/task 2.py
@@ -0,0 +1,8 @@
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+total = 0
+
+for i in range(1, 51):   # loop from 1 to 50
+    total = total + i    # add each number to total
+
+print("The sum of integers from 1 to 50 is:", total)
